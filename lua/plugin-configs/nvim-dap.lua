52f81a584e87d02b08e4f1798168c26ee7e6165d
davemackintosh <me@davemackintosh.co.uk>
2022-05-26 11:44:56 +0100

    remove this silly binding and remap to one more familiar

--- lua/plugin-configs/nvim-dap.lua
+++ lua/plugin-configs/nvim-dap.lua
@@ -14,9 +14,8 @@ dap.adapters.nlua = function(callback, config)
 end
 
 vim.cmd [[
-	nnoremap <silent> <leader>td :lua require('dap-go').debug_test()<CR>
 	nnoremap <silent> <F5> :lua require"dap".continue()<CR>
-	nnoremap <silent> <F10> :lua require"dap".step_over()<CR>
+	nnoremap <silent> <F8> :lua require"dap".step_over()<CR>
 	nnoremap <silent> <F11> :lua require"dap".step_into()<CR>
 	nnoremap <silent> <F12> :lua require"dap".step_out()<CR>
 	nnoremap <silent> <leader>db :lua require"dap".toggle_breakpoint()<CR>
