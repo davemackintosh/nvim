8e5bd6fcb05a72e6a57f3caa632b71c9aae12d86
davemackintosh <me@davemackintosh.co.uk>
2022-09-13 22:25:00 +0100

    Stupid, why remove capabilities Dave?

--- lua/plugin-configs/mason.lua
+++ lua/plugin-configs/mason.lua
@@ -1,3 +1,7 @@
+local capabilities = require("cmp_nvim_lsp").update_capabilities(
+	vim.lsp.protocol.make_client_capabilities()
+)
+
 require("mason").setup {
 	ui = {
 		icons = {
@@ -31,4 +35,12 @@ require("mason-lspconfig").setup {
 		"sumneko_lua",
 		"vim-language-server",
 	},
+}
+
+require("mason-lspconfig").setup_handlers {
+	function(server_name)
+		require("lspconfig")[server_name].setup {
+			capabilities = capabilities,
+		}
+	end,
 }
