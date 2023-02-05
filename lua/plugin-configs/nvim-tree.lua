8c6de7d89b98ff812e64c4b4162bc38fbcbb0ed5
davemackintosh <me@davemackintosh.co.uk>
2023-02-05 20:31:33 +0000

    Some kinda yadda yadda warning about deprecation yadda yadda.
    Meh.

--- lua/plugin-configs/nvim-tree.lua
+++ lua/plugin-configs/nvim-tree.lua
@@ -1,7 +1,6 @@
 require("nvim-tree").setup {
 	disable_netrw = true,
 	hijack_netrw = true,
-	open_on_setup = true,
 	hijack_unnamed_buffer_when_opening = true,
 	ignore_buffer_on_setup = false,
 	update_focused_file = {
