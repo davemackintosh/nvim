d525e375b9340acff3847d25551c7a57d4d825f5
davemackintosh <me@davemackintosh.co.uk>
2023-04-30 22:23:21 +0100

    Move trouble to the right, better for my eyes.

--- lua/plugin-configs/trouble.lua
+++ lua/plugin-configs/trouble.lua
@@ -1,10 +1,3 @@
-require("trouble").setup {}
-
-local mapx = require("mapx").setup {}
-
-mapx.nnoremap("<leader>xx", "<cmd>Trouble<cr>")
-mapx.nnoremap("<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>")
-mapx.nnoremap("<leader>xd", "<cmd>Trouble document_diagnostics<cr>")
-mapx.nnoremap("<leader>xl", "<cmd>Trouble loclist<cr>")
-mapx.nnoremap("<leader>xq", "<cmd>Trouble quickfix<cr>")
-mapx.nnoremap("gR", "<cmd>Trouble lsp_references<cr>")
+require("trouble").setup {
+	position = "right",
+}
