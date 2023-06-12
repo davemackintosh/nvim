2aa068f561979dfbf98fafa73bf6d205c8b7dec2
davemackintosh <me@davemackintosh.co.uk>
2023-06-12 12:14:10 +0100

    small fixes

--- lua/plugin-configs/lsp.lua
+++ lua/plugin-configs/lsp.lua
@@ -13,14 +13,6 @@ lsp_defaults.capabilities = vim.tbl_deep_extend(
 require("mason").setup {}
 require("mason-lspconfig").setup {}
 
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
-
 local function cloneTable(t)
 	local t2 = {}
 	for k, v in pairs(t) do
