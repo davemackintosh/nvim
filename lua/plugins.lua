2a335ab19cb10248bb46c495f8ba5d7cc6a0ed69
davemackintosh <me@davemackintosh.co.uk>
2023-11-04 20:30:30 +0000

    Add Tabnine

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -105,6 +105,21 @@ return {
 			require "plugin-configs.tmux"
 		end,
 	},
+	{
+		"codota/tabnine-nvim",
+		build = "./dl_binaries.sh",
+		config = function()
+			require("tabnine").setup {
+				disable_auto_comment = true,
+				accept_keymap = "<Tab>",
+				dismiss_keymap = "<C-]>",
+				debounce_ms = 800,
+				suggestion_color = { gui = "#808080", cterm = 244 },
+				exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+				log_file_path = nil, -- absolute path to Tabnine log file
+			}
+		end,
+	},
 	--	{
 	--		"akinsho/bufferline.nvim",
 	--		dependencies = "kyazdani42/nvim-web-devicons",
