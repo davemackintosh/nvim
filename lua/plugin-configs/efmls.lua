27a8571cef9eb206ff2a22933a4028ee8f0e0002
davemackintosh <me@davemackintosh.co.uk>
2023-11-21 17:38:45 +0000

    Add goimports formatter

--- lua/plugin-configs/efmls.lua
+++ lua/plugin-configs/efmls.lua
@@ -2,6 +2,13 @@ local lspconfig = require "lspconfig"
 local lsp_defaults = lspconfig.util.default_config
 local languages = require("efmls-configs.defaults").languages()
 
+languages = vim.tbl_extend("force", languages, {
+	go = {
+		require "efmls-configs.linters.golangci_lint",
+		require "efmls-configs.formatters.goimports",
+	},
+})
+
 local efmls_config = {
 	filetypes = vim.tbl_keys(languages),
 	settings = {
