114ca5f1606db93793a97f9b9b280cc57ba759db
davemackintosh <me@davemackintosh.co.uk>
2024-05-17 12:04:50 +0100

    don't bind to tab

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -35,17 +35,7 @@ return {
 		"dcampos/nvim-snippy",
 		dependencies = { "honza/vim-snippets" },
 		config = function()
-			require('snippy').setup({
-				mappings = {
-					is = {
-						['<Tab>'] = 'expand_or_advance',
-						['<S-Tab>'] = 'previous',
-					},
-					nx = {
-						['<leader>x'] = 'cut_text',
-					},
-				},
-			})
+			require('snippy').setup {}
 		end
 	},
 	{
