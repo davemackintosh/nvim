c80288443c1dc1621f262628345c47c2984377df
davemackintosh <me@davemackintosh.co.uk>
2024-06-06 13:47:43 +0100

    Fix golang issues

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -167,6 +167,19 @@ return {
 		end
 	},
 	{
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", 'gomod' },
+	},
+	{
 		"nvim-neotest/neotest",
 		dependencies = {
 			"nvim-neotest/nvim-nio",
